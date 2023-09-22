b415044cd3424a7a094c3e99352d73858fd0645e
BuiQuangTri <10khongquang@gmail.com>
2023-09-22 12:54:29 +0700

    Add files via upload

---
+++ Common/Common/Common/AppEnvironment.swift
@@ -0,0 +1,12 @@
+//
+//  Environment.swift
+//  Common
+//
+//  Created by TriBQ on 19/09/2023.
+//
+
+import Foundation
+
+public struct AppEnvironment {
+    public static let endpoint = "pokeapi.co"
+}
